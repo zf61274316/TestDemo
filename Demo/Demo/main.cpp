a5720d3af95d0dfbda43972a4855d564bf3e7416
zf61274316 <61274316@qq.com>
2017-10-25 14:40:28 +0800

    初始化一个空的C++项目

---
+++ Demo/Demo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Demo
+//
+//  Created by student on 17/10/25.
+//  Copyright © 2017年 zhoufeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
